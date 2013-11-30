23838d3f5cfebc974d00a5ba17087a65a4b5605f
Ivan Wills <ivan.wills@gmail.com>
2013-11-30 23:55:25 +0000

    Prevented undefined error

--- lib/Term/Colour256.pm
+++ lib/Term/Colour256.pm
@@ -10,9 +10,6 @@ use strict;
 use warnings;
 use version;
 use Carp qw/confess longmess/;
-use Scalar::Util;
-use List::Util;
-#use List::MoreUtils;
 use Data::Dumper qw/Dumper/;
 use English qw/ -no_match_vars /;
 use base qw/Exporter/;
@@ -20,7 +17,6 @@ use base qw/Exporter/;
 our $VERSION     = version->new('0.0.1');
 our @EXPORT_OK   = qw/colour coloured color colored push_colour push_color pop_colour pop_color/;
 our %EXPORT_TAGS = ();
-#our @EXPORT      = qw//;
 
 my $global;
 
@@ -30,7 +26,7 @@ sub new {
 }
 
 sub colour {
-    my @colours = map { split /\s+|_/, $_ } @_;
+    my @colours = map { split /\s+|_/, $_ || '0' } @_;
     my $out = '';
 
     for ( my $i = 0; $i < @colours; $i++ ) {
